6a418b2e16fbdca1a52906874b835e0e54e4c5ff
Aimee Oz <aimee.oz.x@gmail.com>
2020-05-26 04:24:47 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase = "Squawk")
-  string = "#{phrase}!"
-  puts string
-  return string
+  puts "#{phrase}!"
+  return phrase
 end
